4dad62df92f301b374d8cad4487944ec438430fe
Олександр <tuhuge32@outlook.com>
2015-11-27 13:54:34 +0200

    Dodel

--- AvaluateTheTeacher1/AvaluateTheTeacher1/Models/Teachers/Voting.cs
+++ AvaluateTheTeacher1/AvaluateTheTeacher1/Models/Teachers/Voting.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,10 +10,16 @@ namespace AvaluateTheTeacher1.Models.Teachers
     {
         public int Id { get; set; }
 
+        [Required]
+        [RegularExpression(@"[2-5]", ErrorMessage = "Оцінка має бути в межах від 2-ці до 5-ки.")]
         public float Interest { get; set; }
 
+        [Required]
+        [RegularExpression(@"[2-5]", ErrorMessage = "Оцінка має бути в межах від 2-ці до 5-ки.")]
         public float Quality { get; set; }
 
+        [Required]
+        [RegularExpression(@"[2-5]", ErrorMessage = "Оцінка має бути в межах від 2-ці до 5-ки.")]
         public float RelevantToStudents { get; set; }
 
         public int? TeacherId { get; set; }
