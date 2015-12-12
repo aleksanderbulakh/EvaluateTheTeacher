ff723c0e700ed30b9ef48e44ee6cb16081d0d347
Олександр <tuhuge32@outlook.com>
2015-12-12 23:59:49 +0200

    Update

--- AvaluateTheTeacher1/AvaluateTheTeacher1/Controllers/HomeController.cs
+++ AvaluateTheTeacher1/AvaluateTheTeacher1/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AvaluateTheTeacher1.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,8 +9,24 @@ namespace AvaluateTheTeacher1.Controllers
 {
     public class HomeController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
         public ActionResult Index()
         {
+            if(DateTime.Now.Day==1)
+            {
+                var query = from list in db.Ratings
+                            select list;
+                
+                foreach(var data in query)
+                {
+                    var rait = db.Ratings.Where(x=>x.TeacherId==data.TeacherId);
+                    foreach(var change in rait)
+                    {
+                        change.PreviousMonth = data.AvgRating;
+                    }
+                    db.SaveChanges();
+                }
+            }
             return View();
         }
 
