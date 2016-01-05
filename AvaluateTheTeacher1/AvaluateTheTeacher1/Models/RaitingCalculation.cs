3cae8edd7f71a7f0b1e97c0182f5777b7fcf2fcc
Олександр <tuhuge32@outlook.com>
2016-01-05 16:23:34 +0200

    recommit

---
+++ AvaluateTheTeacher1/AvaluateTheTeacher1/Models/RaitingCalculation.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AvaluateTheTeacher1.Models
+{
+    public class RaitingCalculation
+    {
+    }
+}
---
+++ AvaluateTheTeacher1/AvaluateTheTeacher1/Models/RaitingManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AvaluateTheTeacher1.Models
+{
+    public class RaitingManager
+    {
+        RaitingManager()
+        { }
+    }
+}
