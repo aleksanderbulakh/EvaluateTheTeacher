0576d85c93fb38945b6577c24995216e4a7af1ce
Олександр <tuhuge32@outlook.com>
2016-01-21 22:08:37 +0200

    промашка

---
+++ AvaluateTheTeacher1/AvaluateTheTeacher1/obj/Release/Package/PackageTmp/Scripts/jquery-validate-voting.js
@@ -0,0 +1,16 @@
+﻿$('input[type="submit"]').click(function () {
+    var index = 0;
+    $("tr").addClass("validate-border");
+    $('input[type="radio"]:checked').each(function () {
+        if (this.value >= 1 && this.value <= 10) {
+            var idSelectedElement = $(this).parent().parent().attr('id');
+            $('tr[id=' + idSelectedElement + ']').children('td:last-child').css('border', 'none');
+            index++;
+        }
+    });
+    $('tfoot td')
+        .css('border', 'none');
+    if (index < 17) {
+        $(".text-danger").text("Дайте відповідь на всі запитання!");
+    }
+});
