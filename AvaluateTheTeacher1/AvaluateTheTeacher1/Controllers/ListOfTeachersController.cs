7f2f3be33424c139d5dac058963bdbbe0b9fa54a
Олександр <tuhuge32@outlook.com>
2016-01-25 10:46:59 +0200

    Рейтинг - не показывать оценку, если голосов меньше 3-х

--- AvaluateTheTeacher1/AvaluateTheTeacher1/Controllers/ListOfTeachersController.cs
+++ AvaluateTheTeacher1/AvaluateTheTeacher1/Controllers/ListOfTeachersController.cs
@@ -9,16 +9,13 @@ using System.Web.Mvc;
 using System.Drawing;
 using AvaluateTheTeacher1.Models;
 using AvaluateTheTeacher1.Models.Teachers;
+using AvaluateTheTeacher1.CodeReview.Models;
 
 namespace AvaluateTheTeacher1.Controllers
 {
 
     public class ListOfTeachersController : Controller
     {
-        private int? SelectedCathedraId { get; set; }
-        private IQueryable<ListOfTeachers> query { get; set; }
-
-        ApplicationDbContext db = new ApplicationDbContext();
         // GET: ListOfTeachers
 
 
@@ -55,60 +52,11 @@ namespace AvaluateTheTeacher1.Controllers
             //    bmp.Save(destFile, frm);
             //}
 
-            if (id == 0 || id == null)
-            {
-                query =
-                    from listCathedras in db.Cathedras
-                    from listRaitings in db.Ratings
-                    from listTeachers in db.Teachers
-                    .Where(listTeachers1 => (listTeachers1.TeacherId == listRaitings.TeacherId) && (listTeachers1.CathedraId == listCathedras.Id) && (listRaitings.AvgRating != 0))
-                    orderby listRaitings.AvgRating descending, listRaitings.CountRaitingVoting descending, listRaitings.TheDifficultyOfTheCourse descending
-                    select new ListOfTeachers()
-                    {
-                        TeacherId = listTeachers.TeacherId,
-                        NameCathedra = listCathedras.NameCathedra,
-                        Name = listTeachers.Name.Substring(0, 1),
-                        SurName = listTeachers.SurName.Substring(0, 1),
-                        LastName = listTeachers.LastName,
-                        PathToPhoto = listTeachers.PathToPhoto,
-                        Description = listTeachers.Description,
-                        AvgRating = listRaitings.AvgRating,
-                        Count = listRaitings.CountRaitingVoting,
-                        TheDifficultyOfTheCourse = listRaitings.TheDifficultyOfTheCourse
-                    };
-            }
-            else
-            {
-                SelectedCathedraId = id;
-                query =
-                    from listCathedras in db.Cathedras
-                    from listRaitings in db.Ratings
-                    from listTeachers in db.Teachers
-                    .Where(listTeachers1 => (listTeachers1.TeacherId == listRaitings.TeacherId) && ((listTeachers1.CathedraId == SelectedCathedraId) && (listTeachers1.CathedraId == listCathedras.Id) && (listRaitings.AvgRating != 0)))
-                    orderby listRaitings.AvgRating descending, listRaitings.CountRaitingVoting descending, listRaitings.TheDifficultyOfTheCourse descending
-                    select new ListOfTeachers()
-                    {
-                        TeacherId = listTeachers.TeacherId,
-                        NameCathedra = listCathedras.NameCathedra,
-                        Name = listTeachers.Name.Substring(0, 1),
-                        SurName = listTeachers.SurName.Substring(0, 1),
-                        LastName = listTeachers.LastName,
-                        PathToPhoto = listTeachers.PathToPhoto,
-                        Description = listTeachers.Description,
-                        AvgRating = listRaitings.AvgRating,
-                        Count = listRaitings.CountRaitingVoting,
-                        TheDifficultyOfTheCourse = listRaitings.TheDifficultyOfTheCourse
-                    };
-            }
-            List<Cathedra> cathedra = db.Cathedras.ToList();
-            cathedra.Insert(0, new Cathedra { NameCathedra = "Всі кафедри", Id = 0 });
-            FilterDataModel data = new FilterDataModel
-            {
-                Teachers = query.ToList(),
-                Cathedras = cathedra
-            };
+            var raitingModel = new RaitingModel();
 
-            return View(data);
+            var raitingDataList = raitingModel.ListOfTeacherData(id);
+
+            return View(raitingDataList);
         }
     }
 }
